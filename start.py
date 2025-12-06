eb5a75cb4d4068c203f5dc6cf6117d5c5ac92e29
dragox134 <94917939+dragox134@users.noreply.github.com>
2025-12-06 21:09:14 +0000

    updated databases

--- start.py
+++ start.py
@@ -2,9 +2,10 @@ from load_and_update import insert_data_into_db
 
 symbols = ["AAPL", "MSFT", "GOOGL", "AMZN", "NVDA", "META", "TSLA", "BRK-B", "JPM", "JNJ"]
 test = ["TSLA"]
+db = "usable_dbs"
 
 
 for i in symbols:
-  insert_data_into_db(i, f"dbs/60_mins/{i}_database_60min", "TIME_SERIES_INTRADAY")
-  insert_data_into_db(i, f"dbs/daily/{i}_database_daily", "TIME_SERIES_DAILY")
+  insert_data_into_db(i, f"{db}/60_mins/{i}_database_60min", "TIME_SERIES_INTRADAY")
+  insert_data_into_db(i, f"{db}/daily/{i}_database_daily", "TIME_SERIES_DAILY")
 
